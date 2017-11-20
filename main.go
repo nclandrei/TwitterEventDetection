0843ac4450865373c6de4c86080d60cf8f763de2
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-11-20 05:17:06 +0000

    Create functions to create clusters and get them by ID

--- main.go
+++ main.go
@@ -12,18 +12,19 @@ import (
 
 // Cluster defines the structure of a cluster composed of multiple tweets
 type Cluster struct {
-	tweets []Tweet
+	ID            int
+	ClusterTweets []Tweet
 }
 
 // Tweet defines the structure of a tweet
 type Tweet struct {
-	clusterID   int
-	namedEntity string
-	tweetID     int
-	userID      int
-	timestampMS int
-	tweetTokens string
-	tweetText   string
+	ClusterID   int
+	NamedEntity string
+	TweetID     int
+	UserID      int
+	TimestampMS int
+	TweetTokens string
+	TweetText   string
 }
 
 func main() {
@@ -38,18 +39,16 @@ func main() {
 			log.Fatal(error)
 		}
 		tweets = append(tweets, Tweet{
-			clusterID:   convertCsvStringToInt(line[0]),
-			namedEntity: line[1],
-			tweetID:     convertCsvStringToInt(line[2]),
-			timestampMS: convertCsvStringToInt(line[3]),
-			userID:      convertCsvStringToInt(line[4]),
-			tweetTokens: line[5],
-			tweetText:   line[6],
+			ClusterID:   convertCsvStringToInt(line[0]),
+			NamedEntity: line[1],
+			TweetID:     convertCsvStringToInt(line[2]),
+			TimestampMS: convertCsvStringToInt(line[3]),
+			UserID:      convertCsvStringToInt(line[4]),
+			TweetTokens: line[5],
+			TweetText:   line[6],
 		})
 	}
-	for _, tweet := range tweets {
-		fmt.Printf(tweet.namedEntity)
-	}
+	createClusters(tweets)
 }
 
 func convertCsvStringToInt(str string) int {
@@ -61,5 +60,38 @@ func convertCsvStringToInt(str string) int {
 }
 
 func createClusters(tweets []Tweet) []Cluster {
-	var clusters = []Cluster{}
+	var clusters []Cluster
+	for _, tweet := range tweets {
+		if !contains(clusters, tweet.ClusterID) {
+			cluster := new(Cluster)
+			cluster.ID = tweet.ClusterID
+			cluster.ClusterTweets = []Tweet{}
+			cluster.ClusterTweets = append(cluster.ClusterTweets, tweet)
+			clusters = append(clusters, *cluster)
+		} else {
+			cluster := getClusterByID(clusters, tweet.ClusterID)
+			cluster.ClusterTweets = append(cluster.ClusterTweets, tweet)
+		}
+	}
+	return clusters
+}
+
+func getClusterByID(clusters []Cluster, id int) Cluster {
+	var cluster Cluster
+	for _, tempCluster := range clusters {
+		if tempCluster.ID == id {
+			cluster = tempCluster
+			break
+		}
+	}
+	return cluster
+}
+
+func contains(clusters []Cluster, clusterID int) bool {
+	for _, cluster := range clusters {
+		if cluster.ClusterTweets[0].ClusterID == clusterID {
+			return true
+		}
+	}
+	return false
 }
