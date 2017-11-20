a01dd163154316bf240acbf85c8d167e3df0e767
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-11-20 09:50:14 +0000

    Create easy to use main function

--- main.go
+++ main.go
@@ -1,24 +1,63 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"sort"
+	"strconv"
 
 	"github.com/nclandrei/TwitterEventDetection/cluster"
 	"github.com/nclandrei/TwitterEventDetection/io"
 )
 
 func main() {
-	tweets := io.ReadFromCSV("clusters.sortedby.clusterid.csv")
-	clusters := cluster.CreateClusters(tweets)
-	clusters = mergeEventsOnNamedEntities(clusters, 3600000)
-	sort.Slice(clusters, func(i, j int) bool {
-		return clusters[i].CentroidTime < clusters[j].CentroidTime
-	})
-	io.WriteToCSV(cluster.ConvertListOfClustersToListOfTweets(clusters), "result.csv")
-	// clusters = filterByNumberOfTweets(clusters, 10)
+	fmt.Println("Welcome! Please type 1 for filtering technique and 2 for merging technique: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	option := scanner.Text()
+	fmt.Printf("%s\n", option)
+	if option == "1" {
+		fmt.Println("Please input source CSV file (needs to be in the same folder as this Go file: ")
+		scanner.Scan()
+		sourceCSVFile := scanner.Text()
+		fmt.Println("Please input number of tweets threshold: ")
+		scanner.Scan()
+		numberOfTweetsStr := scanner.Text()
+		numberOfTweetsInt, _ := strconv.Atoi(numberOfTweetsStr)
+		tweets := io.ReadFromCSV(sourceCSVFile)
+		clusters := cluster.CreateClusters(tweets)
+		clusters = filterByNumberOfTweets(clusters, numberOfTweetsInt)
+		io.WriteToCSV(cluster.ConvertListOfClustersToListOfTweets(clusters), "result.csv")
+		fmt.Println("All done. Your output CSV file is in result.csv")
+	} else if option == "2" {
+		fmt.Println("Please input source CSV file (needs to be in the same folder as this Go file: ")
+		scanner.Scan()
+		sourceCSVFile := scanner.Text()
+		fmt.Println("Please input number of tweets threshold: ")
+		scanner.Scan()
+		numberOfTweetsStr := scanner.Text()
+		numberOfTweetsInt, _ := strconv.Atoi(numberOfTweetsStr)
+		tweets := io.ReadFromCSV(sourceCSVFile)
+		clusters := cluster.CreateClusters(tweets)
+		clusters = filterByNumberOfTweets(clusters, numberOfTweetsInt)
+		io.WriteToCSV(cluster.ConvertListOfClustersToListOfTweets(clusters), "result.csv")
+		fmt.Println("Please input your window interval in milliseconds: ")
+		scanner.Scan()
+		windowInterval := scanner.Text()
+		windowIntervalInt, _ := strconv.Atoi(windowInterval)
+		clusters = mergeEventsOnNamedEntities(clusters, windowIntervalInt)
+		sort.Slice(clusters, func(i, j int) bool {
+			return clusters[i].CentroidTime < clusters[j].CentroidTime
+		})
+		io.WriteToCSV(cluster.ConvertListOfClustersToListOfTweets(clusters), "result.csv")
+
+		fmt.Println("All done. Your output CSV file is in result.csv")
+	} else {
+		fmt.Println("You did not enter neither 1 nor 2. Please try again.")
+	}
 }
 
-//
 func filterByNumberOfTweets(clusters []cluster.Cluster, numberOfTweets int) []cluster.Cluster {
 	for i := len(clusters) - 1; i >= 0; i-- {
 		if len(clusters[i].ClusterTweets) < numberOfTweets {
@@ -28,7 +67,6 @@ func filterByNumberOfTweets(clusters []cluster.Cluster, numberOfTweets int) []cl
 	return clusters
 }
 
-//
 func mergeEventsOnNamedEntities(clusters []cluster.Cluster, windowInterval int) []cluster.Cluster {
 	clusterMap := make(map[string][]cluster.Cluster)
 	for _, cluster := range clusters {
